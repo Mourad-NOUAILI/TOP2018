7d33508c3adc6be86917640288de65df33fd707a
Mourad-NOUAILI <mouradnouaili1@gmail.com>
2017-11-26 10:51:19 +0100

    Add files via upload

---
+++ pre-selection -2/practice-test/le-code/p3.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX_N = 16;
+
+bool found[MAX_N] = {false};
+
+set<int> hasChosen;
+
+int main() {
+  
+  srand(time(NULL));
+
+  int pos = 1;
+  while (!found[pos]) {
+    int r =  (rand() % 3) + 1;
+
+    auto it = hasChosen.find(r);
+    if (it == hasChosen.end()) {
+      hasChosen.insert(r);
+      cout << r << '\n';
+
+      string ans;
+      cin >> ans;
+
+      if (ans.compare("DONE") == 0)
+        break;
+
+      if (ans.compare("YES") == 0) {
+        found[pos] = true;
+         hasChosen.clear();
+         pos++;
+      }
+    }
+    else
+      r =  (rand() % 3) + 1;  
+
+  }
+
+  return 0;
+}
